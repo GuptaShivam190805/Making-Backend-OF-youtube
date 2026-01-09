feea11d07672971e9b4ecbb7206ec78ed5458dda
Shivam Gupta <sg5743006@gmail.com>
2026-01-09 21:15:22 +0530

    add the user Controller

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,45 @@
+import {v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+//configure cloundinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+
+      const response = await cloudinary.uploader.upload(
+            localFilePath, {
+                resource_type: "auto"
+            }
+        ) 
+        console.log("File uploaded on cloudinary. File src: " + response.url)
+        
+        // once the file is uploaded, we would like to delete it from our server
+        fs.unlinkSync(localFilePath)
+        console.log("File uploaded on cloudinary. File src: " + response)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+const deleteFromqCloudinary = async (publicId) => {
+    try {
+        const result = await cloudinary.uploader.destroy(publicId);
+        console.log("Deleted from cloudinary. Public Id, " , publicId);
+    } catch (error) {
+        console.log("Error deleting from cloudinary", error);
+        return null
+    }
+}
+
+export {uploadOnCloudinary, deleteFromqCloudinary}
